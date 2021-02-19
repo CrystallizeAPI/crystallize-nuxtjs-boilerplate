4ca163bd089b64bad80530c4b2b4a08af09a7915
Victor Ribero <victor.ribero3@gmail.com>
2021-02-19 16:42:14 -0700

    Remove unnecessary css and moved to logo.css

--- components/Layout/Header/logo.vue
+++ components/Layout/Header/logo.vue
@@ -2,18 +2,4 @@
   <img src="/logo-shop.svg" alt="" width="207" height="35" />
 </template>
 
-<style scoped>
-  img {
-    display: block;
-    height: 40px;
-    width: 110px;
-    object-fit: contain;
-    z-index: 99;
-  }
-    
-  @media screen and (min-width: 768px) {
-    img {
-      width: 160px;
-    }
-  }
-</style>
+<style scoped src='./logo.css'></style>
