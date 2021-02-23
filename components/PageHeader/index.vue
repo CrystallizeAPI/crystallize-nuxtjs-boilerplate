9d2bdbc1712e36f5636e1235dfd892e59c1aec84
Victor Ribero <victor.ribero3@gmail.com>
2021-02-22 22:46:20 -0700

    Created PageHeader component

---
+++ components/PageHeader/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="page-header">
+    <slot name='before-content' />
+    <h1>{{title}}</h1>
+    <div v-if="description" class="page-header__description">
+      <CrystallizeContentTransformer :data="description" />
+    </div>
+    <slot name='after-content' />
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: Object,
+      required: false,
+    }
+  }
+}
+</script>
+
+<style scoped src='./index.css'></style>
