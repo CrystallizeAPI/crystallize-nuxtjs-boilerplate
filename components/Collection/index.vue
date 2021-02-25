19f94c7e55c0a213b52a935e204d939486cf36f1
Victor Ribero <victor.ribero3@gmail.com>
2021-02-24 18:31:18 -0700

    Created Collection component. The stackable components renders the grid collections composing the collection and the grid

---
+++ components/Collection/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="collection">
+    <h4 class="collection__title">{{ title }}</h4>
+    <div v-if="description" class="collection__description">
+      <CrystallizeContentTransformer :data="description" />
+    </div>
+    <div class="collection__content">
+      <slot />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    /*
+     * The description prop is coupled to how the Crystallize GraphQLtreats text
+     */
+    description: {
+      type: Array,
+      required: false,
+    },
+  },
+};
+</script>
+
+<style scoped src='./index.css'></style>
--- components/Stackable/index.vue
+++ components/Stackable/index.vue
@@ -2,43 +2,39 @@
   <div>
     <div v-for="stack in stacks" :key="stack.id">
       <!-- @TODO: Grid Collection -->
-      <div>
-        <h4>{{ getTitle(stack) }}</h4>
-        <div v-if="getDescription(stack)">
-          <CrystallizeContentTransformer :data="getDescription(stack)" />
-        </div>
+      <Collection :title="getTitle(stack)" :description="getDescription(stack)">
         <div v-for="grid in getGridsFromStack(stack)" :key="grid.name">
           <CrystallizeGrid :grid="grid" />
         </div>
-      </div>
-      
+      </Collection>
+
       <!-- @TODO: Item Collection -->
       <!-- @TODO: Collection This can be a grid collection or item collection -->
       <!-- @TODO: Banner -->
     </div>
   </div>
-
 </template>
 
 <script>
-  export default {
-    props: {
-      stacks: {
-        type: Array,
-        required: true,
-        default: []
-      }
+export default {
+  props: {
+    stacks: {
+      type: Array,
+      required: true,
+      default: [],
     },
-    methods: {
-      getTitle: function(stack) {
-        return stack.components?.find(c => c.name === 'Title')?.content?.text;
-      },
-      getDescription: function(stack) {
-        return stack.components?.find(c => c.name === 'Description')?.content?.json;
-      },
-      getGridsFromStack: function(stack) {
-        return stack.components?.find(c => c.name === 'Grid')?.content?.grids;
-      }
-    }
-  }
+  },
+  methods: {
+    getTitle: function (stack) {
+      return stack.components?.find((c) => c.name === "Title")?.content?.text;
+    },
+    getDescription: function (stack) {
+      return stack.components?.find((c) => c.name === "Description")?.content
+        ?.json;
+    },
+    getGridsFromStack: function (stack) {
+      return stack.components?.find((c) => c.name === "Grid")?.content?.grids;
+    },
+  },
+};
 </script>
