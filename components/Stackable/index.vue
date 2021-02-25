fbe15bcef00483fa5410806e150d4a7d7f6fd41b
Victor Ribero <victor.ribero3@gmail.com>
2021-02-24 18:11:26 -0700

    Stackable renders GridCollection without styles

--- components/Stackable/index.vue
+++ components/Stackable/index.vue
@@ -1,5 +1,23 @@
 <template>
-  
+  <div>
+    <div v-for="stack in stacks" :key="stack.id">
+      <!-- @TODO: Grid Collection -->
+      <div>
+        <h4>{{ getTitle(stack) }}</h4>
+        <div v-if="getDescription(stack)">
+          <CrystallizeContentTransformer :data="getDescription(stack)" />
+        </div>
+        <div v-for="grid in getGridsFromStack(stack)" :key="grid.name">
+          <CrystallizeGrid :grid="grid" />
+        </div>
+      </div>
+      
+      <!-- @TODO: Item Collection -->
+      <!-- @TODO: Collection This can be a grid collection or item collection -->
+      <!-- @TODO: Banner -->
+    </div>
+  </div>
+
 </template>
 
 <script>
@@ -9,6 +27,17 @@
         type: Array,
         required: true,
         default: []
+      }
+    },
+    methods: {
+      getTitle: function(stack) {
+        return stack.components?.find(c => c.name === 'Title')?.content?.text;
+      },
+      getDescription: function(stack) {
+        return stack.components?.find(c => c.name === 'Description')?.content?.json;
+      },
+      getGridsFromStack: function(stack) {
+        return stack.components?.find(c => c.name === 'Grid')?.content?.grids;
       }
     }
   }
