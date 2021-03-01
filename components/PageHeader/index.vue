deb328c8fb6c40dc37035bf1c8dc101b2a42d4a0
Victor Ribero <victor.ribero3@gmail.com>
2021-03-01 13:50:17 -0700

    format

--- components/PageHeader/index.vue
+++ components/PageHeader/index.vue
@@ -1,11 +1,11 @@
 <template>
   <section class="page-header">
-    <slot name='preHeader' />
-    <h1>{{title}}</h1>
+    <slot name="preHeader" />
+    <h1 class="page-header__title">{{ title }}</h1>
     <div v-if="description" class="page-header__description">
       <CrystallizeContentTransformer :data="description" />
     </div>
-    <slot name='postHeader' />
+    <slot name="postHeader" />
   </section>
 </template>
 
@@ -14,18 +14,18 @@ export default {
   props: {
     title: {
       type: String,
-      required: true
+      required: true,
     },
     /*
      * The description prop is coupled to how the Crystallize GraphQLtreats text
-     * 
+     *
      */
     description: {
       type: Array,
       required: false,
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style scoped src='./index.css'></style>
