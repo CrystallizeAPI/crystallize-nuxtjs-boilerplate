8ad37490e0f82a564bab47e0dda5fdefacee908b
Victor Ribero <victor.ribero3@gmail.com>
2021-03-01 09:01:14 -0700

    Created ListFormatProduct and removed unneccesary product related grid-item stuff

--- components/GridItem/index.vue
+++ components/GridItem/index.vue
@@ -7,55 +7,47 @@
     }"
     :class="`grid-item grid-item--${type} ${cellDynamicClass}`"
   >
-    <div v-if="type === 'product'" class="grid-item__inner product">
-      <div class="grid-item__info">
-        <h3 class="grid-item__title">{{ name }}</h3>
-        <GridItemPrice :variant="defaultVariant" />
+    <div class="grid-item__inner">
+      <ListFormatProduct
+        v-if="type === 'product'"
+        :variant="defaultVariant"
+        :name="name"
+        :video="video"
+        :image="image"
+      />
+
+      <div v-if="type === 'document'" class="grid-item__inner">
+        <div class="grid-item__info">
+          <h3 class="grid-item__title">{{ name }}</h3>
+        </div>
+
+        <div class="grid-item__media">
+          <CrystallizeVideo v-if="video" :video="video" />
+          <CrystallizeImage
+            v-else
+            :image="image"
+            :sizes="`(min-width 1024px) ${imageMdWidth}px, 100vw`"
+            :width="1024"
+            class="grid-item__image"
+          />
+        </div>
       </div>
 
-      <div class="grid-item__media">
-        <CrystallizeVideo v-if="video" :video="video" />
-        <CrystallizeImage
-          v-else
-          :image="image"
-          :sizes="`(min-width 1024px) ${imageMdWidth}px, 100vw`"
-          :width="1024"
-          class="grid-item__image"
-        />
-      </div>
-    </div>
+      <div v-if="type === 'folder'" class="grid-item__inner">
+        <div class="grid-item__info">
+          <h3 class="grid-item__title">{{ name }}</h3>
+        </div>
 
-    <div v-if="type === 'document'" class="grid-item__inner">
-      <div class="grid-item__info">
-        <h3 class="grid-item__title">{{ name }}</h3>
-      </div>
-
-      <div class="grid-item__media">
-        <CrystallizeVideo v-if="video" :video="video" />
-        <CrystallizeImage
-          v-else
-          :image="image"
-          :sizes="`(min-width 1024px) ${imageMdWidth}px, 100vw`"
-          :width="1024"
-          class="grid-item__image"
-        />
-      </div>
-    </div>
-
-    <div v-if="type === 'folder'" class="grid-item__inner">
-      <div class="grid-item__info">
-        <h3 class="grid-item__title">{{ name }}</h3>
-      </div>
-
-      <div class="grid-item__media">
-        <CrystallizeVideo v-if="video" :video="video" />
-        <CrystallizeImage
-          v-else
-          :image="image"
-          :sizes="`(min-width 1024px) ${imageMdWidth}px, 100vw`"
-          :width="1024"
-          class="grid-item__image"
-        />
+        <div class="grid-item__media">
+          <CrystallizeVideo v-if="video" :video="video" />
+          <CrystallizeImage
+            v-else
+            :image="image"
+            :sizes="`(min-width 1024px) ${imageMdWidth}px, 100vw`"
+            :width="1024"
+            class="grid-item__image"
+          />
+        </div>
       </div>
     </div>
   </NuxtLink>
---
+++ components/ListFormat/product.vue
@@ -0,0 +1,47 @@
+<template>
+  <article class="list-format-product">
+    <div class="list-format-product__info">
+      <h3 class="list-format-product__title">{{ name }}</h3>
+      <GridItemPrice :variant="variant" />
+    </div>
+
+    <div class="list-format-product__media">
+      <CrystallizeVideo v-if="video" :video="video" />
+      <CrystallizeImage
+        v-else
+        :image="image"
+        :sizes="`(min-width 1024px) ${imageMdWidth}px, 100vw`"
+        :width="1024"
+        class="list-format-product__image"
+      />
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    variant: {
+      type: Object,
+      required: true,
+    },
+    video: {
+      type: Object,
+      required: false,
+    },
+    image: {
+      type: Object,
+      required: false,
+    },
+  },
+  data: function () {
+    return {};
+  },
+};
+</script>
+
+<style scoped src='./product.css'></style>
