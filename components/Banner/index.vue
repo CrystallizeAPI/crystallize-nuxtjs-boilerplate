69098d335bc0964b9d1231b4d50792c2473b747c
Victor Ribero <victor.ribero3@gmail.com>
2021-03-01 23:06:17 -0700

    Banner mostly working. still have to fix a couple of styles

---
+++ components/Banner/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <section
+    class="banner"
+    :class="hasTextAsOverlay ? 'banner--withOverlay' : ''"
+  >
+    <div class="banner__content">
+      <div>
+        <h2 class="banner__title">{{ title }}</h2>
+        <div v-if="description" class="banner__description">
+          <CrystallizeContentTransformer :data="description" />
+        </div>
+        <NuxtLink class="banner__cta" :to="link">
+          {{ linkText }}
+        </NuxtLink>
+      </div>
+    </div>
+    <figure v-if="image" class="banner__media">
+      <CrystallizeImage
+        :image="image"
+        :width="1024"
+        sizes="(max-width: 700px) 100vw, 1200px"
+      />
+    </figure>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: Object,
+      required: true,
+    },
+  },
+  data: function () {
+    const hasFindFunction = Boolean(this.data.components?.find);
+    const c = this.data?.components || [];
+
+    const title =
+      hasFindFunction && c.find((c) => c.name === "Title")?.content?.text;
+    const description =
+      hasFindFunction && c.find((c) => c.name === "Description")?.content?.json;
+    const linkText =
+      hasFindFunction && c.find((c) => c.name === "Link text")?.content?.text;
+    const link =
+      hasFindFunction && c.find((c) => c.name === "Link")?.content?.text;
+    const image =
+      hasFindFunction &&
+      c.find((c) => c.type === "images")?.content?.images?.[0];
+    const hasTextAsOverlay =
+      hasFindFunction &&
+      c.find((c) => c.name === "Add text as overlay")?.content?.value;
+
+    return {
+      title,
+      description,
+      linkText,
+      link: "/shop",
+      image,
+      hasTextAsOverlay: true,
+    };
+  },
+};
+</script>
+
+<style scoped src='./index.css'></style>
