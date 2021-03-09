591f0bd93cd778ab7cd1cc31db76fc4617f7168a
Victor Ribero <victor.ribero3@gmail.com>
2021-03-09 11:25:25 -0700

    Created ListFormat factory

---
+++ components/ListFormat/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <ListFormatProduct v-if="data.type === 'product'" :data="data" />
+  <ListFormatDocument v-else-if="data.type === 'document'" :data="data" />
+  <ListFormatFolder v-else-if="data.type === 'folder'" :data="data" />
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
