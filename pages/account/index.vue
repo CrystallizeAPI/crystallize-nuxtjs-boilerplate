b59154bb7bb1323b69a7b3741762d791bb793ff0
Victor Ribero <victor.ribero3@gmail.com>
2021-03-24 10:01:43 -0700

    Initial login page

---
+++ pages/account/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="account-page">
+    <Container>
+      <template v-if="user.isLoggedIn">
+        <h1 class="account-page-title">Welcome {{ user.email }}</h1>
+        <p>You are logged in</p>
+      </template>
+      <template v-if="!user.isLoggedIn">
+        <h1 class="account-page-title">Login</h1>
+        <div class="account-page__form-wrapper">
+          <form class="account-page__form">
+            <h4 class="account-page__form-title">
+              Enter your email address and we’ll send a magic login link to your
+              inbox.
+            </h4>
+            <div class="account-page__form-displayer">
+              <input
+                type="email"
+                placeholder="Email"
+                class="account-page__input"
+              />
+              <Button class="account-page__submit" alignment="center">
+                Send me a magic link
+              </Button>
+            </div>
+          </form>
+        </div>
+      </template>
+    </Container>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    user: function () {
+      return this.$store.state.authentication;
+    },
+  },
+};
+</script>
+
+<style scoped src='./index.css'></style>
