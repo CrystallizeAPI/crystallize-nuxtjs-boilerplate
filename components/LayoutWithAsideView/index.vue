07cb3db21d54e9f512aa445a64b6d83087110b2e
Victor Ribero <victor.ribero3@gmail.com>
2021-03-29 12:55:15 -0700

    Created LayoutWithAsideView component for displaying the aside

---
+++ components/LayoutWithAsideView/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div :class="moduleStyles">
+    <div
+      v-if="isAsideShow"
+      class="layout-with-aside-view__overlay"
+      @click="handleOverlayClick"
+      aria-hidden="true"
+    />
+    <div class="layout-with-aside-view__aside">
+      <slot name="view-to-toggle" />
+    </div>
+    <div class="layout-with-aside-view__content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    isAsideShow() {
+      return this.$store.state.layout.isAsideShown;
+    },
+    moduleStyles() {
+      const baseClass = "layout-with-aside-view";
+      let classes = [baseClass];
+
+      const { isAsideShown } = this.$store.state.layout;
+      isAsideShown && classes.push(`${baseClass}--is-expanded`);
+
+      return classes.join(" ");
+    },
+  },
+  methods: {
+    handleOverlayClick() {
+      this.$store.dispatch("layout/hideAside");
+    },
+  },
+};
+</script>
+
+<style scoped src='./index.css'></style>
