eb79b8f6eaf688c07017f513cf566075ee147751
Victor Ribero <victor.ribero3@gmail.com>
2021-04-08 13:14:59 -0600

    Stripe payment components now loads Stripe script

--- pages/checkout/Payment/Providers/Stripe/payment.vue
+++ pages/checkout/Payment/Providers/Stripe/payment.vue
@@ -1,9 +1,52 @@
 <template>
-  <div>working</div>
+  <div>
+    <div v-if="isStripeLoaded">stripe is loaded</div>
+    <div v-else>stripe is NOT loaded</div>
+  </div>
 </template>
 
 <script>
+import { serviceApi } from "/lib/service-api";
+
 export default {
-  props: {},
+  data() {
+    return {
+      publishableKey: null,
+      isMounted: false,
+      isStripeLoaded: false,
+    };
+  },
+  async beforeCreate() {
+    const { data } = await serviceApi({
+      query: `
+      {
+        paymentProviders {
+          stripe {
+            config
+          }
+        }
+      }
+    `,
+    });
+
+    this.publishableKey = data.paymentProviders.stripe.config.publishableKey;
+  },
+  mounted() {
+    this.isMounted = true;
+  },
+  head() {
+    return {
+      script: [
+        {
+          hid: "stripe",
+          src: "https://js.stripe.com/v3/",
+          defer: true,
+          callback: () => {
+            this.isStripeLoaded = true;
+          },
+        },
+      ],
+    };
+  },
 };
 </script>
