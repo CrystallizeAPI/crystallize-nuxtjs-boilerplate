fa5efd41c193bb986920ac70b09534e022d59007
Victor Ribero <victor.ribero3@gmail.com>
2021-04-14 14:37:43 -0600

    Solved issue of not updating the VariantSelectorImage of an attribute option when selecting a variant that has many attributes

--- components/Crystallize/Image/index.vue
+++ components/Crystallize/Image/index.vue
@@ -69,63 +69,108 @@ export default {
     },
   },
   data() {
-    const image = this.image;
-    const { variants, caption } = image;
-    const vars = Boolean(variants) ? variants : [];
-    // Separate the image variants by their respective types
-    const std = vars?.filter((v) => v.url && !v.url.endsWith(".webp"));
-    const webp = vars?.filter((v) => v.url && v.url.endsWith(".webp"));
-    const srcSet = std.map(this.getVariantSrc).join(", ");
-    const srcSetWebp = webp.map(this.getVariantSrc).join(", ");
-    const captionString = caption?.html?.[0] || caption?.plainText?.[0] || "";
-
-    // Determine the file extension for the original image
-    let originalFileExtension = "jpeg";
-    const srcToGetOriginalFileExtension = image.url || std[0]?.url;
-    if (srcToGetOriginalFileExtension) {
-      ({
-        groups: { name: this.originalFileExtension },
-      } = srcToGetOriginalFileExtension.match(/\.(?<name>[^.]+)$/));
-
-      // Provide correct mime type for jpg
-      if (originalFileExtension === "jpg") {
-        originalFileExtension = "jpeg";
-      }
-    }
-
-    /**
-     * Set the fallback url to be the standard variant
-     * if it exists, or the original image url.
-     * The fallback will be used if the browser does
-     * not support srcset: https://caniuse.com/srcset
-     */
-    const src = std[0]?.url || image.url;
-
-    // Get the image dimensions
-    let biggestImage = vars[0];
-    if (vars.length > 0) {
-      biggestImage = vars.reduce(function (acc, v) {
-        if (!acc.width || v.width > acc.width) {
-          return v;
-        }
-        return acc;
-      }, vars[0]);
-    }
-
-    return {
-      altText: this.alt || this.image.altText,
+    const {
+      altText,
       originalFileExtension,
       srcSetWebp,
       srcSet,
       src,
-      bigWidth: biggestImage?.width,
-      bigHeight: biggestImage?.height,
+      bigWidth,
+      bigHeight,
+      captionString,
+    } = this.getImageData(this.image);
+
+    return {
+      altText,
+      originalFileExtension,
+      srcSetWebp,
+      srcSet,
+      src,
+      bigWidth,
+      bigHeight,
       captionString,
     };
   },
   methods: {
     getVariantSrc(variant) {
       return `${variant.url} ${variant.width}w`;
+    },
+    getImageData(image) {
+      const { variants, caption } = image;
+      const vars = Boolean(variants) ? variants : [];
+      // Separate the image variants by their respective types
+      const std = vars?.filter((v) => v.url && !v.url.endsWith(".webp"));
+      const webp = vars?.filter((v) => v.url && v.url.endsWith(".webp"));
+      const srcSet = std.map(this.getVariantSrc).join(", ");
+      const srcSetWebp = webp.map(this.getVariantSrc).join(", ");
+      const captionString = caption?.html?.[0] || caption?.plainText?.[0] || "";
+
+      // Determine the file extension for the original image
+      let originalFileExtension = "jpeg";
+      const srcToGetOriginalFileExtension = image.url || std[0]?.url;
+      if (srcToGetOriginalFileExtension) {
+        ({
+          groups: { name: this.originalFileExtension },
+        } = srcToGetOriginalFileExtension.match(/\.(?<name>[^.]+)$/));
+
+        // Provide correct mime type for jpg
+        if (originalFileExtension === "jpg") {
+          originalFileExtension = "jpeg";
+        }
+      }
+
+      /**
+       * Set the fallback url to be the standard variant
+       * if it exists, or the original image url.
+       * The fallback will be used if the browser does
+       * not support srcset: https://caniuse.com/srcset
+       */
+      const src = std[0]?.url || image.url;
+
+      // Get the image dimensions
+      let biggestImage = vars[0];
+      if (vars.length > 0) {
+        biggestImage = vars.reduce(function (acc, v) {
+          if (!acc.width || v.width > acc.width) {
+            return v;
+          }
+          return acc;
+        }, vars[0]);
+      }
+
+      return {
+        altText: this.alt || image.altText,
+        originalFileExtension,
+        srcSetWebp,
+        srcSet,
+        src,
+        bigWidth: biggestImage?.width,
+        bigHeight: biggestImage?.height,
+        captionString,
+      };
+    },
+  },
+  watch: {
+    image(newImage) {
+      const {
+        altText,
+        originalFileExtension,
+        srcSetWebp,
+        srcSet,
+        src,
+        bigWidth,
+        bigHeight,
+        captionString,
+      } = this.getImageData(newImage);
+
+      this.altText = altText;
+      this.originalFileExtension = originalFileExtension;
+      this.srcSetWebp = srcSetWebp;
+      this.srcSet = srcSet;
+      this.src = src;
+      this.bigWidth = bigWidth;
+      this.bigHeight = bigHeight;
+      this.captionString = captionString;
     },
   },
 };
