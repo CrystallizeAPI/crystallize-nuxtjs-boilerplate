979fe5e1b0766ce5ed8f847ef9d15709dc57223f
Victor Ribero <victor.ribero3@gmail.com>
2021-04-13 18:29:42 -0600

    remove custom variable for testing purposes

--- plugins/preview.client.js
+++ plugins/preview.client.js
@@ -1,8 +1,5 @@
 export default function ({ query, enablePreview }) {
   if (query.preview) {
-    // register global variable
-    enablePreview({
-      isPreview: true
-    })
+    enablePreview()
   }
 }
